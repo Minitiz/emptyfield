86d26d5a55b67972f667071e703b16c57274b2fd
pyl <pierre-yves.liegeois@w6d.io>
2021-01-05 18:29:49 +0100

    surrounded test not finish
    Signed-off-by: pyl <pierre-yves.liegeois@w6d.io>

--- pkg/tag/tag_test.go
+++ pkg/tag/tag_test.go
@@ -2,8 +2,6 @@ package tag
 
 import (
 	"testing"
-
-	opt "github.com/minitiz/emptyfield/pkg/options"
 )
 
 type datatest struct {
@@ -52,42 +50,42 @@ type noTagTest struct {
 	data string
 }
 
-func Test_OmitEmpty(t *testing.T) {
+// func Test_OmitEmpty(t *testing.T) {
 
-	tests := []struct {
-		name string
-		data interface{}
-		opt  *opt.Option
-		want bool
-	}{
+// 	tests := []struct {
+// 		name string
+// 		data interface{}
+// 		opt  *opt.Option
+// 		want bool
+// 	}{
 
-		//presence omitempty on field tag
-		{"test 1", fieldTagTest{"test"}, &opt.Options{}, true},
-		{"test 2", jsonTagTest{"test"}, &opt.Options{true, false}, true},
-		{"test 3", jsonTagTest{"test"}, &opt.Options{}, false},
-		{"test 4", noTagTest{"test"}, &opt.Options{}, false},
-	}
+// 		//presence omitempty on field tag
+// 		{"test 1", fieldTagTest{"test"}, &opt.Options{}, true},
+// 		{"test 2", jsonTagTest{"test"}, &opt.Options{true, false}, true},
+// 		{"test 3", jsonTagTest{"test"}, &opt.Options{}, false},
+// 		{"test 4", noTagTest{"test"}, &opt.Options{}, false},
+// 	}
 
-	//presence omitempty on json tag but opt nil
-	// {
-	// 	name: "test 2",
-	// 	data struct{
-	// 		test string `json:",omitempty`
-	// 	}{
-	// 		"test",
-	// 	},
-	// 	opt: nil,
-	// 	want false,
-	// },
-	//presence omitempty on json tag and opt say to parse it
+// 	//presence omitempty on json tag but opt nil
+// 	// {
+// 	// 	name: "test 2",
+// 	// 	data struct{
+// 	// 		test string `json:",omitempty`
+// 	// 	}{
+// 	// 		"test",
+// 	// 	},
+// 	// 	opt: nil,
+// 	// 	want false,
+// 	// },
+// 	//presence omitempty on json tag and opt say to parse it
 
-	// TODO: Add test cases.
+// 	// TODO: Add test cases.
 
-	// for _, tt := range tests {
-	// 	t.Run(tt.name, func(t *testing.T) {
-	// 		if got := OmitEmptyTag(reflect.TypeOf(tt.data).Field(0).Tag,tt.opt) ; got != tt.want {
-	// 			t.Errorf("tagOptions.Contains() = %v, want %v", got, tt.want)
-	// 		}
-	// 	})
-	// }
-}
+// 	// for _, tt := range tests {
+// 	// 	t.Run(tt.name, func(t *testing.T) {
+// 	// 		if got := OmitEmptyTag(reflect.TypeOf(tt.data).Field(0).Tag,tt.opt) ; got != tt.want {
+// 	// 			t.Errorf("tagOptions.Contains() = %v, want %v", got, tt.want)
+// 	// 		}
+// 	// 	})
+// 	// }
+// }
